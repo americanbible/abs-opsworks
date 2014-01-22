e4208633c6efc094ad3a8972e03ce1fb555eeb70
Brad Olson <brado@movedbylight.com>
2014-01-22 14:13:00 -0500

    renamed main module

--- abs_scripture_library/metadata.rb
+++ abs_scripture_library/metadata.rb
@@ -2,4 +2,4 @@ maintainer "American Bible Society"
 description "Handles setup peculiar to the ABS Scripture Library Application"
 version "0.1"
 
-recipe "abs_scripture_library::config_server", "Configure the Ruby on Rails application server"
+recipe "abs_scripture_library::configure", "Configure the Ruby on Rails application server"
