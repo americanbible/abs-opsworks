7484142b60a32eb4af12d729da45bf3023a327be
mbradshawabs <mbradshaw@americanbible.org>
2014-03-17 11:26:10 -0500

    Add ssh private key recipe

---
+++ opsworks-ssh-private-key/metadata.rb
@@ -0,0 +1,7 @@
+name        "ssh_private_users"
+description 'Add private ssh key to deploy user'
+maintainer  "ABS"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'opsworks_initial_setup'
