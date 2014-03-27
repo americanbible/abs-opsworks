bedbf0eb57d734bef13afb4df4135454696dfb55
mbradshawabs <mbradshaw@americanbible.org>
2014-03-27 11:31:48 -0500

    more renaming

--- opsworks-repo-key/metadata.rb
+++ opsworks-repo-key/metadata.rb
@@ -1,9 +1,7 @@
-name        "ssh_deploy_repo_key"
-description 'Add private ssh key to deploy user for a private github repo'
+name        "opsworks_deploy_private_key"
+description 'Add private ssh key to deploy user.  Useful for a private github repo.'
 maintainer  "ABS"
 license     "Apache 2.0"
 version     "1.0.0"
 
 depends 'opsworks_initial_setup'
-
-recipe "opsworks_deploy_key::add", "Add a github ssh key for private repos"
