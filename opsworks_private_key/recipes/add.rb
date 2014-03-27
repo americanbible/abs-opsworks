dac1455998c3aa0c11df9589de4f890d8e724f02
mbradshawabs <mbradshaw@americanbible.org>
2014-03-27 15:07:58 -0500

    finalize private key recipe, and add a README per Bryce on the process of using oauth tokens for private gems

--- opsworks_private_key/recipes/add.rb
+++ opsworks_private_key/recipes/add.rb
@@ -2,6 +2,7 @@ Chef::Log.info("adding private repo key for user deploy")
 
 node[:private_key].each do |name, key_contents|
 	file "/home/deploy/.ssh/#{name}" do
+    # represent newlines with \n
     content key_contents.gsub(/\\n/, "\n")
     owner 'deploy'
     group 'deploy'
