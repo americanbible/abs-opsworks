2a177ff2bea22366e0a30d31327adc4cc31cbc96
mbradshawabs <mbradshaw@americanbible.org>
2014-03-27 11:23:44 -0500

    retrying repo key recipe

---
+++ opsworks-repo-key/metadata.rb
@@ -0,0 +1,9 @@
+name        "ssh_deploy_repo_key"
+description 'Add private ssh key to deploy user for a private github repo'
+maintainer  "ABS"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'opsworks_initial_setup'
+
+recipe "abs_deploy_key::add", "Add a github ssh key for private repos"
--- opsworks-ssh-private-key/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        "ssh_deploy_private_ssh"
-description 'Add private ssh key to deploy user'
-maintainer  "ABS"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends 'opsworks_initial_setup'
