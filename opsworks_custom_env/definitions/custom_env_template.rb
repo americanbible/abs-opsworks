558dfec53fd8bd6aba81351253577daa4f2bcc6e
mbradshawabs <mbradshaw@americanbible.org>
2014-03-31 11:06:40 -0500

    try to modify profile

--- opsworks_custom_env/definitions/custom_env_template.rb
+++ opsworks_custom_env/definitions/custom_env_template.rb
@@ -12,6 +12,18 @@ define :custom_env_template do
     ENV[key] = value
   end
 
+  template "/home/deploy/.profile" do
+    source "profile.erb"
+    owner params[:deploy][:user]
+    group params[:deploy][:group]
+    mode "0660"
+    variables :env => params[:env]
+
+    only_if do
+      File.exists?("#{params[:deploy][:deploy_to]}/shared/config")
+    end
+  end
+
   template "#{params[:deploy][:deploy_to]}/shared/config/application.yml" do
     source "application.yml.erb"
     owner params[:deploy][:user]
